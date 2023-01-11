9d063576b9004c39f957d8545eaa12c73f7c442c
causticrystals <peanutpc173@gmail.com>
2023-01-11 13:23:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Dice Roll Simulator Assignment
 
 import random
+
+n = 1
+while n <= 5:
+    # Flip a Coin
+    randNum = random.randrange(1, 3)
+    if randNum == 1:
+        print("Heads")
+    else:
+        print("Tails")
+
+    # Increment n
+    n += 1
+
+print("Goodbye")
