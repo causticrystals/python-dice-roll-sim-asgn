48378e7ce943091f44da4aec284d86ce9c1a54fe
causticrystals <peanutpc173@gmail.com>
2023-01-11 13:16:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 # Dice Roll Simulator Assignment
 
 import random
-
-# Menu
-print("Dice Roll Simulator Menu")
-print("1. Roll Dice Once")
-print("2. Roll Dice 5 Times")
-print("3. Roll Dice 'n' Times")
-print("4. Roll Dice until Snake Eyes")
-print("5. Exit")
